19219b32241a225fff0e8477d1aeb7ba7f2e4827
Hekal Jakub (256985) <256985@vutbr.cz>
2024-04-24 10:51:18 +0200

    Flood fill created

--- flood_fill.py
+++ flood_fill.py
@@ -1,17 +1,34 @@
 import matplotlib.pyplot as plt
 
 
+def flood_fill(img, x, y):
+    if 0 <= y < len(img) and 0 <= x < len(img[0]):
+        v = int(img[y, x])
+        if v == 1:
+            img[y, x] = 2
+
+            plt.imshow(img, cmap='gray')
+            plt.show(block=False)
+            plt.pause(0.001)
+            plt.clf()
+
+            img = flood_fill(img, x + 1, y)
+            img = flood_fill(img, x - 1, y)
+            img = flood_fill(img, x, y + 1)
+            img = flood_fill(img, x, y - 1)
+
+    return img
+
+
 def main():
-    img = plt.imread("files/img0.png")[:, :, 0]
-    # img = plt.imread("files/img1.png")[:, :, 0]
-    # img = plt.imread("files/img2.png")[:, :, 0]
 
-    # img = floodfill(img, 0, 0)
+    for i in range(3):
+        img = plt.imread(f"files/img{i}.png")[:, :, 0]
 
-    plt.imshow(img, cmap="gray")
-    plt.show(block=False)
-    plt.pause(5)
-    plt.clf()
+        img = flood_fill(img, 0, 0)
+
+        plt.imshow(img, cmap='gray')
+        plt.show()
 
 
 if __name__ == "__main__":
