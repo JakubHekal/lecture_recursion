2a5c1dd903dd38fc06a7a0c9f4ac88f54a72643c
Hekal Jakub (256985) <256985@vutbr.cz>
2024-04-24 09:10:47 +0200

    Fibo completed

--- fibo.py
+++ fibo.py
@@ -1,5 +1,13 @@
+def recursive_nth_fibo(n):
+    if n < 0:
+        return None
+    if n < 2:
+        return n
+    return recursive_nth_fibo(n-1) + recursive_nth_fibo(n-2)
+
 
 def main():
+    print(recursive_nth_fibo(3))
     pass
 
 
